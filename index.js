85487d22ec39ab72ee6ed45ed2e4bfa24ae874fe
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 16:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){kittens.push(name);return kittens}
