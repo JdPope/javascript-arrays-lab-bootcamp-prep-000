bd77762769fb9d12c303180718e0325feff7ceeb
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 16:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyAppendKitten(name){kittens.push(name);return kittens}
 function destructivelyPrependKitten(name){kittens.unshift(name);return kittens}
 function destructivelyRemoveLastKitten(){kittens.pop(); return kittens}
 function destructivelyRemoveFirstKitten(){kittens.shift(); return kittens}
-function appendKitten(name){kittens = [...kittens, name]; return kittens}
+function appendKitten(name){return [...kittens, name] }
 function prependKitten(name){kittens = [name, ...kittens]; return kittens}
 function removeLastKitten(){kittens = kittens.slice(0,kittens.length-1);return kittens}
