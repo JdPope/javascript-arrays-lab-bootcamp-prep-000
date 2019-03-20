050e8c1281f22d1a4ab116a602a6c550dd459654
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 15:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){kittens.push(name);return kittens}
 function destructivelyPrependKitten(name){kittens.unshift(name);return kittens}
 function destructivelyRemoveLastKitten(){kittens.pop(); return kittens}
-function destructivelyRemovesFirstKitten()
+function destructivelyRemovesFirstKitten(){kittens.shift(); return kittens}
